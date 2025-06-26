c37f2339ee6db0131168d6b65fe6ad01348115a6
TJ-SIVA <160028154+TJ-SIVA@users.noreply.github.com>
2025-06-26 10:58:38 +0530

    Create Perfect_or_Not.java
    add file 2

---
+++ Perfect_or_Not.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int fact = 0;
+        if(number < 0){
+            
+            System.out.println("Invalid Input");
+        }
+        
+        else{
+            
+            for(int i=1;i<number;i++){
+                
+                if(number%i==0){
+                    
+                    fact += i;
+                    
+                    
+                }
+            }
+            
+            if(fact == number){
+                
+                System.out.println("The Number "+ (float)number +" is a Perfect Number.");
+            }
+            
+            else{
+                
+                 System.out.println("The Number "+ (float)number +" is a Not Perfect Number.");
+            }
+        }
+        
+    }
+}
